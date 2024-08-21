f7915c386f4eb554f1b5c0f15a10b534f9965b51
KMT <gn02205502@gmail.com>
2024-08-21 12:58:19 +0800

    add code

--- sketch_feb17a/sketch_feb17a.ino
+++ sketch_feb17a/sketch_feb17a.ino
@@ -5,10 +5,9 @@
 enum ClockMode { Nornal, SettingSecond, SettingMinute, SettingHour };
 static bool Pressed = true, Unpressed = false;
 const int led_output_pin_ = 12;//共用小時的大平台pin
-bool current_led_turn_on_ = false;
-ShiftRegister second_(6,5,7);
+ShiftRegister second_(6,7,5);//跟下列兩個的接線不同
 ShiftRegister minut_(9,10,8);
-ShiftRegister hour_(12,13,11);
+ShiftRegister hour_(12,13,11);//跟minute一樣
 
 Ds1302 real_time_clock_(4/*enable_pin*/, 2/*clock_pin*/, 3/*data_pin*/);
 
@@ -20,19 +19,20 @@ ClockMode current_mode_;
 //正常模式：更新時間
 void SetRealTimeClock();
 void UpdataTimeIfNextSecond();
-void ProcessSettingClockMode();
 
 //清理輝光管
-void ProcessCleanTube(const bool back_btn, const bool front_btn);
-void CheckIfExecuteCleanTubeMode(const bool back_btn, const bool front_btn);
+void ProcessCleanTube(const bool back_set_btn, const bool front_add_btn);
+void CheckIfExecuteCleanTubeMode(
+  const bool back_set_btn, const bool front_add_btn);
 void AutoCleanTube();
 void CleanTube();
 
 //檢查是否設定時間、更新時間
-void ProcessSettingTime(const bool back_set_btn, const bool front_add_bton);
-void CheckIfExecuteSettingTimeMode(const bool back_button, const bool front_button);
-void CheckIfSwitchSettingTime(const bool back_button);
-void ListenAddButtonToSetTime(const bool back_button, const bool front_button);
+void ProcessSettingTime(const bool back_set_btn, const bool front_add_btn);
+void CheckIfExecuteSettingTimeMode(
+  const bool back_set_btn, const bool front_add_btn);
+void CheckIfSwitchSettingTime(const bool back_set_btn);
+void ListenAddButtonToSetTime(const bool back_set_btn, const bool front_add_btn);
 void AdjustTime();
 void BlinkTube();
 unsigned long last_clean_tube_time_millsecond_;
@@ -42,7 +42,7 @@ void setup(){
   Serial.begin(9600);
   last_clean_tube_time_millsecond_ = millis();
   current_mode_ = ClockMode::Nornal; // 當前設定模式
-  RandomizeNextCleaning();
+  RandomizeNextCleaningTime();
   SetRealTimeClock();
   pinMode(button_input_mode_back_pin_, INPUT);
   pinMode(button_input_add_front_pin_, INPUT);
@@ -50,11 +50,13 @@ void setup(){
 
 void loop(){
   UpdataTimeIfNextSecond();
-  const bool back_setting_btn = analogRead(button_input_mode_back_pin_) == 0;
-  const bool front_adding_btn = analogRead(button_input_add_front_pin_) == 0;
+  const int back_value = analogRead(button_input_mode_back_pin_);
+  const int front_value = analogRead(button_input_add_front_pin_);
+  const bool back_setting_btn = back_value < 50;
+  const bool front_adding_btn = front_value < 50;
   ProcessCleanTube(back_setting_btn, front_adding_btn);
   ProcessSettingTime(back_setting_btn, front_adding_btn);
-  delay(20);
+  delay(10);
 }
 
 void ProcessCleanTube(const bool back_btn, const bool front_btn){
@@ -69,6 +71,7 @@ void UpdataTimeIfNextSecond(){
   if (last_second_ == now.second){
     return;
   }
+  static bool current_led_turn_on_ = false;
   if(current_led_turn_on_){
     digitalWrite(led_output_pin_, HIGH);
   }
@@ -84,6 +87,7 @@ void UpdataTimeIfNextSecond(){
   second_.SetClockTime(now.second);
   minut_.SetClockTime(now.minute);
   hour_.SetClockTime(now.hour);
+  Serial.println(String(now.hour) + ":" + String(now.minute) + ":" + String(now.second));
 }
 
 void SetRealTimeClock(){
@@ -96,14 +100,9 @@ void SetRealTimeClock(){
   }
 }
 
-unsigned long press_2_button_millsecond_ = 0; // 記錄按下的開始時間
-unsigned long last_auto_clean_millsecond_ = 0; // 記錄上次清除時間 自動執行： 15 ~ 30分鐘
-bool is_cleaning_mode_ = false; // 是否處於輝光管清除模式
-static const int tube_clean_time_ = 100;    
-const byte tube_show_numbers_[10] = {11, 66, 22, 55, 0, 44, 77, 99, 88, 33};
-
-
 void CheckIfExecuteCleanTubeMode(const bool back_button, const bool front_button){
+  static unsigned long press_2_button_millsecond_ = 0; // 記錄按下的開始時間
+  static bool is_cleaning_mode_ = false; // 是否處於輝光管清除模式
   unsigned long press_duration;
   // 檢測是否同時按下按鈕
   if (back_button == Pressed && front_button == Pressed) {
@@ -121,7 +120,6 @@ void CheckIfExecuteCleanTubeMode(const bool back_button, const bool front_button
         CleanTube();
         is_cleaning_mode_ = false; // 清除模式結束
         press_2_button_millsecond_ = 0; // 重置按下時間，以便下次按下
-        last_auto_clean_millsecond_ = 0;// 如果手動清除, 則自動清除的計時也歸０
       }
     }
   } else {
@@ -134,11 +132,11 @@ void CleanTube(){
   // 啟動輝光管保護，點亮一段時間
   int current_clean_time = 0;        
   float clean_delay_time = 100;
-  bool on = false;
-  while(current_clean_time < tube_clean_time_){
-    int show_num = tube_show_numbers_[current_clean_time % 10];
-    digitalWrite(led_output_pin_, on);
-    on = !on;
+  const int tube_clean_time = 100;
+  //由管子的後到前顯示數字  
+  const byte tube_show_numbers[10] = {11, 66, 22, 55, 0, 44, 77, 99, 88, 33};
+  while(current_clean_time < tube_clean_time){
+    int show_num = tube_show_numbers[current_clean_time % 10];
     second_.SetClockTime(show_num);
     minut_.SetClockTime(show_num);
     hour_.SetClockTime(show_num);
@@ -146,15 +144,17 @@ void CleanTube(){
     clean_delay_time -= 0.9;
     current_clean_time++;
   }
+  UpdataTimeIfNextSecond();//清理完管子後, 馬上顯示當前時間
 }
 
 unsigned long target_time_minutes_ = 0;  // 目標時間，單位為分鐘
-void RandomizeNextCleaning(){// 隨機選擇15至30分鐘間3的倍數的時間
+void RandomizeNextCleaningTime(){// 隨機選擇15至30分鐘間3的倍數的時間
   int min_minutes = 15;
   int max_minutes = 30;
   int interval = 3;
   target_time_minutes_ = 
     random(min_minutes / interval, (max_minutes / interval) + 1) * interval;
+  Serial.println("target_time_minutes_"+String(target_time_minutes_));
 }
 
 void AutoCleanTube(){
@@ -166,16 +166,14 @@ void AutoCleanTube(){
   if (time_diff_minutes == target_time_minutes_){
     CleanTube();
     last_clean_tube_time_millsecond_ = millis();
-    RandomizeNextCleaning();
+    RandomizeNextCleaningTime();
   }
 }
 
-unsigned long press_front_setting_button_millsecond_ = 0;
-unsigned long press_back_add_button_millsecond_ = 0; 
 //*************************************設定時間*************************
 bool last_back_set_btn_;
 bool last_front_add_btn_;
-unsigned long setting_mode_press_time_ = 0; // 記錄進入設定模式按鈕按下的時間
+unsigned long setting_mode_press_time_ = millis(); // 記錄進入設定模式按鈕按下的時間
 //*********************************設定時間******************************
 unsigned long last_blink_millsecond_ = 0;
 
@@ -198,9 +196,13 @@ unsigned long setting_mode_unpress_millsecond_ = 0;
 void CheckIfExecuteSettingTimeMode(bool back_setting_btn, bool front_add_btn){
   // 如果在正常模式下長按back_button 且add按鈕沒按(清理模式會按著2顆按鈕)，則進入設定模式
   if (back_setting_btn == Pressed && front_add_btn == Unpressed){
+
+    const unsigned long press_duration = millis() - setting_mode_press_time_;
     if (setting_mode_press_time_ == 0) {
       setting_mode_press_time_ = millis(); // 開始計時
-    } else if (millis() - setting_mode_press_time_ > 3000) { // 長按超過3秒
+      Serial.println("press setting button duration :" + String(press_duration));
+      
+    } else if (press_duration > 3000) { // 長按超過3秒
       Serial.println("Entering setting second mode");
       last_blink_millsecond_ = millis();
       current_mode_ = ClockMode::SettingSecond; // 進入設定秒數模式
@@ -220,21 +222,22 @@ void CheckIfSwitchSettingTime(const bool back_set_btn){
     return;
   }
   setting_mode_unpress_millsecond_ = millis();
-  delay(1); // 簡單的防抖動
   // 循環通過設定模式
   if (current_mode_ == SettingSecond){
     current_mode_ = SettingMinute;
     Serial.println("Switch to set minute mode");
+    delay(5);//防抖動
   }
   else if (current_mode_ == SettingMinute){
     current_mode_ = SettingHour;
     Serial.println("Switch to set hour mode");
+    delay(5);//防抖動
   } 
   else if (current_mode_ == SettingHour){
     current_mode_ = SettingSecond;
     Serial.println("Switch to set second mode");
+    delay(5);//防抖動
   } 
-  Serial.println("last_blink_millsecond_~~~"+String( last_blink_millsecond_));
 
   last_blink_millsecond_ = millis();
   last_back_set_btn_ = back_set_btn;
@@ -258,7 +261,7 @@ void ListenAddButtonToSetTime(const bool back_button, const bool front_add_btn){
   if (front_add_btn == Pressed){
     setting_mode_unpress_millsecond_ = millis();//有按按鍵,更新上次按下的時間
     AdjustTime();
-    delay(1); // 簡單的防抖動
+    delay(1);//防抖動
   }
 }
 
